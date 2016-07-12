34020fc5843c550c32e5324eb7968cae3eb5970d
nicothebrush <nicola.riolini@gmail.com>
2016-07-12 12:07:34 +0200

    Correct force procedure checking line and header

--- force_sale/force.py
+++ force_sale/force.py
@@ -80,17 +80,19 @@ class SaleOrder(orm.Model):
         # pool used:
         sol_pool = self.pool.get('sale.order.line')
         
+        # Search line used:
         sol_ids = sol_pool.search(cr, uid, [
             ('order_id', '=', ids[0]),
             ], context=context)
 
+        # Update line quantity:
         sol_update = {}
+
         for line in sol_pool.browse(cr, uid, sol_ids, context=context):
             sol_update[line.id] = round(
                 line.product_uom_qty * force_value / 100.0, 0)
-        
-        # upate context to force CL (SL?)
-        for item_id in sol_update:
+                
+        for item_id in sol_update: # upate context to force CL (SL?)
             sol_pool.write(cr, uid, [item_id], {
                 'product_uom_force_qty': sol_update[item_id], 
                 }, context=context)
@@ -105,23 +107,39 @@ class SaleOrder(orm.Model):
 
         # pool used:
         sol_pool = self.pool.get('sale.order.line')
+
+        # Check list:
+        sol_all = []
+        sol_partial = []
         
         sol_ids = sol_pool.search(cr, uid, [
             ('order_id', '=', ids[0]),
-            ('product_uom_force_qty', '>', 0),
+            #('product_uom_force_qty', '>', 0), # not filter for check list!
             ], context=context)
 
         ctx = context.copy()
         sol_update = {}
         for line in sol_pool.browse(cr, uid, sol_ids, context=context):
+            force_qty = line.product_uom_force_qty
+            order_qty = line.product_uom_qty
+            
+            if not force_qty:
+                continue
+
+            # Check force value for line analysis
+            qty = order_qty - force_qty
+            if qty:
+                sol_partial.append(line.id)
+            else:
+                sol_all.append(line.id)
+            
             # TODO check that is product or 
             ctx['force_persistent'] = True
             sol_pool._recreate_production_sol_move(cr, uid, [line.id], 
                 context=ctx)
 
             # No extra sale forced down:
-            sol_update[
-                line.id] = line.product_uom_qty - line.product_uom_force_qty
+            sol_update[line.id] = qty
             # No extra production forced down:
         
         # upate context to force CL (SL?)
@@ -132,6 +150,17 @@ class SaleOrder(orm.Model):
                 'product_uom_maked_sync_qty': qty,                
                 'product_uom_force_qty': 0, 
                 }, context=context)
+
+        # Clean line:    
+        if sol_all:
+            sol_pool.unlink(cr, uid, sol_all, context=context)
+        
+        # Clena order:    
+        if not sol_partial:
+            # Put in cancel state for unlink operation:
+            self.write(cr, uid, ids, {
+                'state': 'cancel'}, context=context)
+            self.unlink(cr, uid, ids, context=context)
         return True
 
     _columns = {
