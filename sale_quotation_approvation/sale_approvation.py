9993004604763dc3493fc3dc194a34b6079bd7c9
nicothebrush <nicola.riolini@gmail.com>
2025-02-18 17:26:37 +0100

    [ADD] Module: sale_quotation_approvation

---
+++ sale_quotation_approvation/sale_approvation.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+#
+#    Copyright (C) 2001-2014 Micronaet SRL (<http://www.micronaet.it>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+import os
+import sys
+import logging
+import openerp
+import openerp.netsvc as netsvc
+import openerp.addons.decimal_precision as dp
+from openerp.osv import fields, osv, expression, orm
+from datetime import datetime, timedelta
+from dateutil.relativedelta import relativedelta
+from openerp import SUPERUSER_ID, api
+from openerp import tools
+from openerp.tools.translate import _
+from openerp.tools.float_utils import float_round as round
+from openerp.tools import (DEFAULT_SERVER_DATE_FORMAT,
+    DEFAULT_SERVER_DATETIME_FORMAT,
+    DATETIME_FORMATS_MAP,
+    float_compare)
+
+
+_logger = logging.getLogger(__name__)
+
+
+class SaleOrder(orm.Model):
+    """ Model name: SaleOrder
+    """
+    _inherit = 'sale.order'
+
+    def action_button_request_approve(self):
+        """ Set order for request approvation
+        """
+        return self.write(cr, uid, ids, {
+            'request_approvation': True,
+        }, context=context)
+
+    _columns = {
+        'request_approvation': fields.boolean('Richiesta approvazione'),
+        }
